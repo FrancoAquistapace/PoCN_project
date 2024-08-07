e3cf93ac1b29896c77953d7b8a546a8486e4bdc1
Franco Aquistapace <francoaquistapace@192.168.1.50>
2024-08-07 21:02:55 +0200

    Fixed functions and added minimal model generator function.

--- code/task_6/tools.py
+++ code/task_6/tools.py
@@ -154,14 +154,15 @@ def stochastic_growth_step(graph, m, e, seed=42):
             1 - e). Must be between 0 and 1.
         seed : int (optional)
             Random seed used during growth process.
+            Default is 42.
     Output:
         Returns a grown version of the given graph, using
-        a hub-hub repulsion mode. In this case, all of the
-        previous edges of the netowrk are erased. Then, m*k 
-        new nodes are added connected to each previous node
-        with degree k. Finally the old connections are replaced
-        by connections between new neighbors of the old nodes, 
-        respectively.
+        a sotchastic growth mode. In this case, the previous 
+        edges of the netowrk are erased with probability 1 - e. 
+        Then, m*k new nodes are added connected to each previous 
+        node with degree k. Finally the deleted connections are 
+        replaced by connections between new neighbors of the old 
+        nodes, respectively.
     '''
     # Check that e is between 0 and 1
     if e < 0 or e > 1:
@@ -215,6 +216,65 @@ def stochastic_growth_step(graph, m, e, seed=42):
             G_new.add_edge(u_new, v_new)
 
     return G_new
+
+
+# Function to generate a minimal-model graph
+# according to specifications
+def minimal_model_graph(init_N, n_iters, m, e, seed=42):
+    '''
+    Params:
+        init_N : int 
+            Number of nodes for the initial star graph.
+            Must be greater than 1.
+        n_iters : int
+            Number of growth iterations to perform. Must
+            be greater than 0.
+        m : int
+            Multiplicative constant for number of new 
+            nodes. Must be greater than 0.
+        e : float 
+            Determines the fraction of Mode I growth
+            (equal to e) and Mode II growth (given by
+            1 - e). Must be between 0 and 1.
+        seed : int (optional)
+            Random seed used during growth process. 
+            Default is 42.
+    Output:
+        Returns a graph grown according to the minimal model
+        defined in the work by Song-Havlin-Makse. The network
+        is initialized as a star graph with init_N nodes. Then,
+        n_iters growth iterations are performed, each of them
+        with a stochastic combination of Modes I and II. This
+        is regulated by the Mode I probability e. Additionally,
+        a dictionary with relevant data recorded at each growth
+        iteration is returned.
+    '''
+    # Check for correct parameters
+    if init_N <= 1:
+        print('Error: init_N must be greater than 1.')
+        return
+    if n_iters <= 0:
+        print('Error: n_iters must be greater than 0.')
+        return
+
+    # Initialize graph and data
+    G = star_graph(init_N)
+    data = {'N': [G.number_of_nodes()],
+            'L': [nx.diameter(G)]}
+
+    # Perform growth process
+    for i in range(n_iters):
+        # Use stochastic growth
+        if i == 0:
+            G = stochastic_growth_step(G, m=m, e=e, seed=seed)
+        else:
+            G = stochastic_growth_step(G, m=m, e=e, seed=seed)
+        # Save iteration data
+        data['N'].append(G.number_of_nodes())
+        data['L'].append(nx.diameter(G))
+
+    # Return graph and data
+    return G, data
 # -----------------------------------
 
 
@@ -277,7 +337,10 @@ def P_joint_deg(measures):
         for k_2 in k_2_vals:
             # Get counts and save them to matrix
             k_2_counts = np.sum(k_1_pairs[:,1] == k_2)
-            A[k_1-1, k_2-1] += k_2_counts
+            A[k_1 - 1, k_2 - 1] *= 0
+            A[k_1 - 1, k_2 - 1] += k_2_counts
+            A[k_2 - 1, k_1 - 1] *= 0
+            A[k_2 - 1, k_1 - 1] += k_2_counts
         
     # Normalize by totality of measures to get frequency
     A = A / np.sum(A)
