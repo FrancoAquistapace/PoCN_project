8974d95606b6bde183d3ab901ffbe98d63c4072e
Franco Aquistapace <francoaquistapace@192.168.1.50>
2024-09-24 12:34:12 +0200

    Initialized production script for task 6, and made small changes to the report latex.

---
+++ code/task_6/production.py
@@ -0,0 +1,9 @@
+'''
+Here we will generate the data that is needed to reproduce the results for the 
+Song-Havlin-Makse paper.
+'''
+# ------ Import scripts and packages ---------
+import numpy as np 
+import networkx as nx
+from tools import *
+# --------------------------------------------
