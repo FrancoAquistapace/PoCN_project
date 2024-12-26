49e28334d3c3ea6a93f2c5ad5fd3ceab51b415c8
Franco Aquistapace <francoaquistapace@192.168.1.50>
2024-12-26 20:01:10 +0100

    Initialized tools script for task 15.

---
+++ code/task_15/tools.py
@@ -0,0 +1,9 @@
+'''
+Here we will define all of the functions and tools that we need in order to reproduce the graphs
+and results for the papers about sandpile dynamics on complex networks.
+'''
+# ------ Necessary packages ---------
+import numpy as np 
+import pandas as pd
+import networkx as nx
+# -----------------------------------
