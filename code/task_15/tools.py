1e9d7bf43d423b1254725d04e53110adb668df04
Franco Aquistapace <francoaquistapace@192.168.1.50>
2025-01-17 21:17:07 +0100

    Developed sandpile simulation class in task 15.

--- code/task_15/tools.py
+++ code/task_15/tools.py
@@ -115,6 +115,218 @@ def get_lcc(graph):
 
 # ---------- Simulation -------------
 
+class SandpileSim(object):
+    def __init__(self, graph, f):
+        '''
+        Params:
+            graph : networkx Graph
+                Network to use as substrate for the simulation. The
+                network should not have any node with degree 0 or 1.
+            f : float
+                Probability of losing a grain on a given transfer 
+                between nodes.
+        '''
+        # System attributes
+        # Network
+        self.graph = graph
+        self.nodes = np.array(list(graph.nodes()))
+        self.nodes_idx = np.arange(len(self.nodes))
+        self.node_map = {n:i for i,n in enumerate(self.nodes)}
+        # Threshold values
+        self.thresh = np.zeros((len(self.nodes,)))
+        # Fraction of grains lost per transfer
+        self.f = f
+
+        # Initialize simulation information:
+        # Number of avalanche events
+        self.n_events = 0
+        # Avalanche area
+        self.A = []
+        # Avalanche size
+        self.S = []
+        # Number of toppled grains
+        self.G = []
+        # Duration of the avalanche
+        self.T = []
+
+        # Current load of the system
+        self.grains = np.zeros((len(self.nodes),))
+
+    # Function to initialize threshold values
+    def init_thresholds(self, mode='degree'):
+        '''
+        Params:
+            mode : str or int (optional)
+                Mode of assigning the threshold value to each node in the 
+                network. Default is "degree", which sets the threshold of 
+                each node as its degree. If an int value is given, then
+                the thresholds are set uniformly to that value.
+        Output:
+            Initializes the self.thresh attribute of the system, which 
+            contains the threshold value for each node in the network.
+        '''
+        # If default, then use network degrees as threshold:
+        if mode == 'degree':
+            for i, n in enumerate(self.nodes):
+                self.thresh[i] += self.graph.degree(n)
+
+        elif type(mode) == type(1):
+            for i, n in enumerate(self.nodes):
+                self.thresh[i] += mode
+
+        else:
+            print('Warning: the threshold mode given is not accepted.')
+
+
+    # Function that adds a grain to a random node of the system
+    def add_grain(self):
+        '''
+        Output:
+            Adds a grain to a random node of the system. Each node
+            is selected with uniform probability.
+        '''
+        # Select random node
+        i_node = np.random.randint(0, len(self.nodes))
+
+        # Add corresponding grain to current load
+        self.grains[i_node] += 1
+
+
+    # Function to check for avalanche threshold
+    def avalanche_check(self):
+        '''
+        Output:
+            Checks if the system is currently in an avalanche 
+            event. Returns a bool accordingly.
+        '''
+        # Check for avalanche
+        threshold_check = np.sum(self.grains >= self.thresh)
+        if threshold_check > 0:
+            return True
+        return False
+
+    
+    # Function to run the simulation
+    def run(self, steps, verbose=True, every=1000, events=None):
+        '''
+        Params:
+            steps : int
+                Number of maximum simulation steps to perform.
+            verbose : bool (optional)
+                Whether to print simulation information. Set 
+                to True by default.
+            every : int (optional)
+                Amount of simulation steps in between information
+                updates printed. Default is 1000 iterations.
+            events : int (optional)
+                Number of avalanche events at which to stop the
+                simulation early. Not used by default.
+        Output:
+            Runs the Bak-Tang-Wiesenfeld sandpile model on the 
+            given network. After it is finished, the following
+            avalanche metrics are stored:
+                - Number of events
+                - Per event:
+                    - Avalanche area (A)
+                    - Avalanche size (S)
+                    - Number of toppled grains (G)
+                    - Duration of the avalanche (T)
+        '''
+        # Start timing
+        t1 = time.time()
+        
+        # Centinel variables
+        finished = False
+        in_avalanche = False
+        i_step = 0
+
+        # Run loop
+        while not finished:
+            # Increase step
+            i_step += 1
+
+            # Check for printing frequency
+            if verbose and i_step % every == 0:
+                print('Step %s of %s. Observed avalanches: %s (max. %s).' % \
+                      (i_step, steps, self.n_events, events))
+
+            in_avalanche = self.avalanche_check()
+
+            # If not in avalanche, increase grain load
+            if not in_avalanche:
+                self.add_grain()
+            
+            # If in avalanche, enter avalanche process until
+            # completion
+            else:
+                # Init avalanche metrics
+                part_nodes = [] # Nodes participating
+                S = 0
+                G = 0
+                T = 0
+                while in_avalanche:
+                    # Increase avalanche duration on each iteration
+                    T += 1
+                    
+                    # Iterate over toppled nodes to generate a new
+                    # state
+                    grains_delta = np.zeros((len(self.nodes),))
+
+                    # Get toppled nodes
+                    t_mask = self.grains >= self.thresh
+                    toppled = self.nodes[t_mask]
+                    toppled_idx = self.nodes_idx[t_mask]
+                    part_nodes.extend(list(toppled_idx))
+                    S += len(toppled)
+                    for i, nt in enumerate(toppled):
+                        t_idx = toppled_idx[i]
+
+                        # Get degree and neighbors of toppled node
+                        deg = self.graph.degree(nt)
+                        neigh_idx = [self.node_map[n]\
+                                     for n in self.graph.neighbors(nt)]
+
+                        # Remove k_i grains from toppled node
+                        grains_delta[t_idx] -= deg
+                        G += deg
+
+                        # Add one grain to each neighbor with prob 1-f
+                        probs = np.random.uniform(size=deg)
+                        p_mask = (probs > self.f).astype('int')
+                        for j, neigh in enumerate(neigh_idx):
+                            grains_delta[neigh] += p_mask[j]
+
+                    # Add delta to current state to get new state
+                    new_grains = self.grains + grains_delta
+                    self.grains = new_grains
+                
+                    # Check if avalanche is finished
+                    in_avalanche = self.avalanche_check()
+
+                # Increase avalanche count and save results
+                self.n_events += 1
+                self.A.append(np.unique(part_nodes).shape[0])
+                self.S.append(S)
+                self.G.append(G)
+                self.T.append(T)
+            
+            # Check for termination
+            if i_step >= steps:
+                finished = True
+            if events != None:
+                if self.n_events >= events:
+                    finished = True
+
+        # Finish timing
+        t2 = time.time()
+
+        if verbose:
+            print('Simulation finished in: %.1f minutes' % ((t2 - t1)/60))
+
+        # Return relevant esults
+        res = {'A': self.A, 'S': self.S, 
+               'G': self.G, 'T': self.T}
+        return res
 
 # -----------------------------------
 
