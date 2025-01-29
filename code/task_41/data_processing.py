9d7db64a654ac44ba39df59f83483b5d145f8bad
Franco Aquistapace <francoaquistapace@192.168.1.50>
2025-01-29 13:02:12 +0100

    Added function to check if there is missing data for a given city.

--- code/task_41/data_processing.py
+++ code/task_41/data_processing.py
@@ -64,6 +64,24 @@ def data_from_city_id(city_id, datasets=all_data):
     return res
 
 
+# Function that checks if either station data or section data is missing
+def check_missing_data(city_data):
+    '''
+    Params:
+        city_data : dict
+            Data of a given city, with at least "stations" and
+            "sections" keys.
+    Output:
+        Returns True if either the stations data or the sections
+        data is missing for the given city, and returns False
+        otherwise.
+    '''
+    no_stations = city_data['stations'].shape[0] == 0
+    no_sections = city_data['sections'].shape[0] == 0
+    if no_stations or no_sections:
+        return True
+        
+    return False
 # -------------------------------------------
 
 
