3b1678eb0dec403f87bd3abc3a2bfa6ecdb7b7fa
Franco Aquistapace <francoaquistapace@192.168.1.50>
2025-02-03 18:22:17 +0100

    Added section to find unique cities in the data for visualization, in task 41.

--- code/task_41/visualization.py
+++ code/task_41/visualization.py
@@ -2,6 +2,7 @@
 Here, we will analyse the results and produce the necessary plots.
 '''
 # ------ Import scripts and packages ---------
+import os
 import numpy as np 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -9,3 +10,13 @@ from scipy.optimize import curve_fit
 import networkx as nx
 from tools import *
 # --------------------------------------------
+
+
+# ----------- Data input ----------------
+# Find unique city names
+data_path = '../../data/task_41/'
+city_names = [p[-1] for p in os.walk(data_path)][0]
+city_names = [s.replace('_nodes.csv', '') for s in city_names if '_nodes' in s]
+
+print('Found %d unique cities in data' % len(city_names))
+# ---------------------------------------
