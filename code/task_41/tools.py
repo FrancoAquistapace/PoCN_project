1c47e62ef0597a74cf67517210bc7d43f7fc3616
Franco Aquistapace <francoaquistapace@192.168.1.50>
2025-02-03 17:53:47 +0100

    Initialized tools and visualization scripts in task 41.

---
+++ code/task_41/tools.py
@@ -0,0 +1,20 @@
+'''
+Here we will define all of the functions and tools that we need in order to read and
+analyse the networks generated from the citylines dataset.
+'''
+# ------ Necessary packages ---------
+import numpy as np 
+import pandas as pd
+import networkx as nx
+import time
+# -----------------------------------
+
+
+# ----------- Analysis --------------
+
+# -----------------------------------
+
+
+# --------- Miscellaneous -----------
+
+# -----------------------------------
---
+++ code/task_41/visualization.py
@@ -0,0 +1,11 @@
+'''
+Here, we will analyse the results and produce the necessary plots.
+'''
+# ------ Import scripts and packages ---------
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import networkx as nx
+from tools import *
+# --------------------------------------------
