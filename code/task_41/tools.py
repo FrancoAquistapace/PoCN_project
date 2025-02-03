9b2cdcc3d76e6f51c620b7c92104354124f9dc5b
Franco Aquistapace <francoaquistapace@192.168.1.50>
2025-02-03 18:09:53 +0100

    Added function to read city nodes and edges from data, in task 41.

--- code/task_41/tools.py
+++ code/task_41/tools.py
@@ -15,6 +15,37 @@ import time
 # -----------------------------------
 
 
+
 # --------- Miscellaneous -----------
+
+# Function to read nodes and edges from a city into a graph
+def read_city_network(path):
+    '''
+    Params:
+        path : str
+            Path of the files "_nodes.csv" and "_edges.csv"
+            with the data of a given city.
+    Output:
+        Returns a networkx Graph built with the nodes and 
+        edges extracted from the data of the given city.
+    '''
+    # First read nodes and edges as DataFrames
+    nodes = pd.read_csv(path + '_nodes.csv')
+    edges = pd.read_csv(path + '_edges.csv')
+
+    # Init empty network
+    G = nx.Graph()
+
+    # Add nodes
+    G.add_nodes_from(nodes['nodeID'])
+
+    # Add edges
+    for i in range(edges.shape[0]):   
+        e = edges.iloc[i]
+        u,v = e['nodeID_from'], e['nodeID_to']
+        G.add_edge(u,v)
+
+    return G
+
 
 # -----------------------------------
